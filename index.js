68b1c52e2b78d8510965d4c0b3279a400954372a
Z-kamili <zakariakamili97@gmail.com>
2022-11-30 11:35:42 +0000

    change url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017')
+mongoose.connect('mongodb://127.0.0.1/playground')
    .then(() => console.log('Connected to MongoDB...'))
    .catch(err => console.log('Could not connect to MongoDB...',err));
 
