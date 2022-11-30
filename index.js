ecbd1b26a54b513c9175e295244bc31e55f3010a
Z-kamili <zakariakamili97@gmail.com>
2022-11-30 11:44:19 +0000

    add schema

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ mongoose.connect('mongodb://127.0.0.1/playground')
    .then(() => console.log('Connected to MongoDB...'))
    .catch(err => console.log('Could not connect to MongoDB...',err));
 
+
+   const courseShema = new mongoose.Schema({
+      name:String,
+      author:String,
+      tags:[String],
+      date:{type:Date,default:Date.now},
+      isPublished:Boolean
+   });
+
