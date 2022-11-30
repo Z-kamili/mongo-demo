ad85230931d5501af041b2260b0463be3556220a
Z-kamili <zakariakamili97@gmail.com>
2022-11-30 11:31:37 +0000

    fix problem

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mongoose =  require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://127.0.0.1:27017')
    .then(() => console.log('Connected to MongoDB...'))
    .catch(err => console.log('Could not connect to MongoDB...',err));
+
